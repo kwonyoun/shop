959b9e331fcabf4c8ac2f8624ee212fff28ea18c
kwon <kyjyj6334@gmail.com>
2025-06-20 16:06:52 +0900

    feat: Add crawling starbucks store list and insert to db

---
+++ src/main/java/com/example/coffe/shop/controller/StoreController.java
@@ -0,0 +1,36 @@
+package com.example.coffe.shop.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import com.example.coffe.shop.dto.StoreDto;
+import com.example.coffe.shop.service.StoreService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("store")
+public class StoreController {
+
+        private final StoreService storeService;
+
+        @GetMapping
+        public String getStoreList(Model model) {
+            List<StoreDto> list = storeService.findAll();
+            model.addAttribute("list", list);
+            return "store";
+        }
+
+        @GetMapping("map")
+        public String getStoreMap() {
+            return "storeMap";
+        }
+        
+        
+    
+}
